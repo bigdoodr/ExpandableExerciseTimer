37d10bb85a0c03c2785affa164149aff1c39d232
bigdoodr <37274306+bigdoodr@users.noreply.github.com>
2025-12-06 16:06:02 -0600

    Initial Commit

---
+++ ExerciseTimer/ExerciseTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseTimerApp.swift
+//  ExerciseTimer
+//
+//  Created by Casey Scruggs on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExerciseTimerTests/ExerciseTimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ExerciseTimerTests.swift
+//  ExerciseTimerTests
+//
+//  Created by Casey Scruggs on 12/6/25.
+//
+
+import Testing
+
+struct ExerciseTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
